a05b890c33aa1dcce1bd06f88645e5d5e3346db2
Srinithis22 <srinithis22@karunya.edu.in>
2025-04-29 09:48:29 +0530

    Add files via upload

---
+++ test_apif.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "http://127.0.0.1:5000/predict"
+data = {"features": [63, 1, 3, 145, 233, 1, 0, 150, 0, 2.3, 0, 0, 1]}
+
+response = requests.post(url, json=data)
+print(response.json())
